bcc24cceb0ad87f8fdfe4097da154ae050c3dce3
tutuat <153525161+tutuat@users.noreply.github.com>
2025-05-24 23:51:52 +0200

    Ajout des boutons pour naviguer dans le carousel

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const nextButton = document.getElementById('next');
 const slides = document.querySelectorAll('ul li');
 
 let currentIndex = 0;
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  });
+  
+  prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+  });
+  
+  showSlide(currentIndex);
