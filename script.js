c52f2470eb7000ffe12586cc4b94f3dd5a40da6d
tutuat <153525161+tutuat@users.noreply.github.com>
2025-05-25 00:04:39 +0200

    Ajout de la fonction showSlide pour gérer l’affichage des images et css de base

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const nextButton = document.getElementById('next');
 const slides = document.querySelectorAll('ul li');
 
 let currentIndex = 0;
-
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.setAttribute('aria-hidden', i !== index);
+    });
+}
 nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % slides.length;
     showSlide(currentIndex);
